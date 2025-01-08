3f0df34b7fdd5f17f4125afa285c4d735bf25b5b
Dan Barton <danbarton2000@gmail.com>
2025-01-08 22:59:29 +0000

    Completed Step 3. Changed backend to server. Added health check.

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,70 @@
+use std::collections::HashSet;
 use tokio::net::TcpStream;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex, RwLock};
+use std::time::Duration;
+
+type Servers = Arc<RwLock<HashSet<String>>>;
+
+pub struct HealthCheck{
+    servers: HashSet<String>,
+    healthy_servers: Servers
+}
+
+impl HealthCheck  {
+    pub fn new(servers: HashSet<String>, healthy_servers: Servers) -> HealthCheck {
+        HealthCheck {
+            servers,
+            healthy_servers
+        }
+    }
+
+    pub async fn health_check(&mut self) {
+        for server_addr in &self.servers {
+            let server_addr = server_addr.clone();
+            let healthy_servers = self.healthy_servers.clone();
+            tokio::spawn(async move {
+                let mut interval = tokio::time::interval(Duration::from_secs(5));
+                loop {
+                    interval.tick().await;
+                    if let Err(_) = TcpStream::connect(&server_addr).await {
+                        eprintln!("Failed to create stream for {server_addr}");
+                        healthy_servers.write().unwrap().retain(|x| *x != server_addr);
+                    } else {
+                        healthy_servers.write().unwrap().insert(server_addr.clone());
+                    }
+                }
+            });
+        }
+    }
+}
 
 pub struct RoundRobin {
-    backends: Vec<String>,
+    servers: Servers,
     current: Mutex<usize>
 }
 
-impl RoundRobin {
-    pub fn new(backends: Vec<String>) -> RoundRobin {
+impl RoundRobin  {
+    pub fn new(servers: Servers) -> RoundRobin {
         RoundRobin {
-            backends,
+            servers,
             current: Mutex::new(0)
         }
     }
 
-    pub fn next_backend(&self) -> Option<String> {
+    pub fn next_server(&self) -> Option<String> {
         let mut current = self.current.lock().unwrap();
-        if self.backends.is_empty() {
+        if self.servers.read().unwrap().is_empty() {
             None
         } else {
-            let backend = self.backends[*current].clone();
-            *current = (*current + 1) % self.backends.len();
+            let backend = self.servers.read().unwrap().iter().nth(*current)?.clone();
+            *current = (*current + 1) % self.servers.read().unwrap().len();
             Some(backend)
         }
     }
 }
 
-pub async fn process(mut inbound: TcpStream, backend_addr: String) -> std::io::Result<()> {
-    let mut outbound = TcpStream::connect(backend_addr).await?;
+pub async fn process(mut inbound: TcpStream, server_addr: String) -> std::io::Result<()> {
+    let mut outbound = TcpStream::connect(server_addr).await?;
     let (mut read_in, mut write_in) = inbound.split();
     let (mut read_out, mut write_out) = outbound.split();
 
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
-use std::sync::Arc;
+use std::collections::HashSet;
+use std::sync::{Arc, RwLock};
 use tokio::net::TcpListener;
 use clap::Parser;
-use load_balancer_rs::{process, RoundRobin};
+use load_balancer_rs::{process, HealthCheck, RoundRobin};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let listener = TcpListener::bind(args.bind_address).await?;
-    let round_robin = Arc::new(RoundRobin::new(args.servers));
+    let healthy_servers = Arc::new(RwLock::new(HashSet::new()));
+
+    let round_robin = Arc::new(RoundRobin::new(healthy_servers.clone()));
+
+    let mut health_check = HealthCheck::new(HashSet::from_iter(args.servers), healthy_servers.clone());
+    health_check.health_check().await;
 
     loop {
         let (stream, _) = listener.accept().await?;
         let round_robin = round_robin.clone();
 
         tokio::spawn(async move {
-            if let Some(backend_addr) = round_robin.next_backend() {
-                println!("Request sent to {backend_addr}");
-                if let Err(e) = process(stream, backend_addr).await {
+            if let Some(server_addr) = round_robin.next_server() {
+                println!("Request sent to {server_addr}");
+                if let Err(e) = process(stream, server_addr).await {
                     eprintln!("Failed to process request: {}", e);
                 }
             } else {
